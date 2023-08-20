f80b0ca40b15d1069265a289d5761d9d28013451
itsgreatness <dzhao650@gmail.com>
2023-08-20 13:51:39 -0500

    New files

---
+++ WindowsAdminCheck EchoOff.bat
@@ -0,0 +1,15 @@
+@echo off
+FOR /F %%G IN ('net localgroup administrators ^| findstr /vc:"The command completed successfully." ^| ^
+findstr /vc:^%%USERNAME^%% ^| ^
+findstr /vc:"Alias name     administrators" ^| ^
+findstr /vc:"Comment        Administrators have complete and unrestricted access to the computer/domain" ^| ^
+findstr /vc:"Members" ^| ^
+findstr /vc:"-------------------------------------------------------------------------------"') ^
+DO net localgroup administrators /delete %%G
+:more_input
+	set "input="
+	set /p input= Type an administrator:
+	IF DEFINED input (
+		net localgroup administrators /add %input%
+		goto :more_input
+	)
